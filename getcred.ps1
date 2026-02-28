4fc5b909d0dd24540f04f4d3fb5618e4fb90aac1
Sajjad <sajjadr742@gmail.com>
2026-02-28 18:28:32 +0500

    Remove unintended script

--- getcred.ps1
+++
@@ -1,9 +0,0 @@
-[void][Windows.Security.Credentials.PasswordVault, Windows.Security.Credentials, ContentType = WindowsRuntime]
-$vault = New-Object Windows.Security.Credentials.PasswordVault
-$creds = $vault.RetrieveAll()
-foreach ($c in $creds) {
-    if ($c.Resource -like '*github*') {
-        $c.RetrievePassword()
-        Write-Host "User: $($c.UserName), Password: $($c.Password)"
-    }
-}
